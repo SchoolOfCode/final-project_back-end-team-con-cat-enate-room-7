1e6cbddb0405b35c05b3b63717db98c6e1fbdc23
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-07-26 16:19:19 +0100

    get all symptoms and post requests working - delete and get by user id in progress

--- models/symptomsModels.js
+++ models/symptomsModels.js
@@ -1,16 +1,46 @@
 import { query } from "../db/index.js";
 
-export async function getSymptomsByUserId(id) {
-  const data = await query(`SELECT * FROM symptoms WHERE user_id = ${id}`);
-
+export async function getAllSymptoms() {
+  const data = await query(`SELECT * FROM symptoms;`);
   return data.rows;
 }
 
+export async function getSymptomsByUserId(id) {
+	const data = await query(`SELECT * FROM symptoms WHERE user_id = ${id}`);
+
+	return data.rows;
+}
+
 export async function createNewSymptom(newSymptom) {
-  const { user_id, pet_id, incident_id, symptoms, symptoms_id, date, time, description } = newSymptom;
-  const data = await query(
-    `INSERT INTO public.symptoms (user_id, pet_id, incident_id, symptoms, symptoms_id, date, time, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
-    [user_id, pet_id, incident_id, symptoms, symptoms_id, date, time, description]
-  );
+	const {
+		user_id,
+		pet_id,
+		incident_id,
+		symptoms,
+		symptoms_id,
+		date,
+		time,
+		description,
+	} = newSymptom;
+	const data = await query(
+		`INSERT INTO symptoms (user_id, pet_id, incident_id, symptoms, symptoms_id, date, time, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
+		[
+			user_id,
+			pet_id,
+			incident_id,
+			symptoms,
+			symptoms_id,
+			date,
+			time,
+			description,
+		]
+	);
+	// const result = await query(`SELECT * FROM symptoms;`);
+	return data.rows;
+}
+
+export async function deleteSymptomByID(id) {
+  await query(`DELETE FROM symptoms WHERE symptoms_id = ${id}`)
+  const data = await query(`SELECT * FROM symptoms;`);
   return data.rows;
 }
