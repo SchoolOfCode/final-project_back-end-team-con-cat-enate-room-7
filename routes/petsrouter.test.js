e92bbd67277573780018af92cbf9ba80982dbca7
JackC91 <jackjcherry@hotmail.co.uk>
2022-07-28 12:06:51 +0100

    all three http methods for /pets route tested and working correctly

--- routes/petsrouter.test.js
+++ routes/petsrouter.test.js
@@ -9,11 +9,11 @@ describe("tests that the Pets routes, are returning all data from DB as well as 
     expect(response.statusCode).toEqual(200);
   });
 
-  test("posting new data on to pets database", async function () {
+  test("posting new data on to pets database, asserting correct response body and status code", async function () {
     await new Promise((resolve) => setTimeout(() => resolve(), 2000));
     const response = await request(app).post("/pets").send({
       user_id: "3",
-      pet_id: "3",
+      pet_id: "5",
       name: "Tall Cat",
       species: true,
       breed: "Tabby",
@@ -37,4 +37,34 @@ describe("tests that the Pets routes, are returning all data from DB as well as 
       ],
     });
   });
-});
+
+  test("updating pet entry on database, asserting correct response body and status code", async function () {
+    await new Promise((resolve) => setTimeout(() => resolve(), 2000));
+    const response = await request(app).put("/pets/5").send({
+      user_id: "3",
+      pet_id: "5",
+      name: "Short Cat",
+      species: true,
+      breed: "Tabby",
+      age: 37,
+      weight: 50,
+    });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toStrictEqual({
+      success: true,
+      message: "Updated pet info",
+      payload: [
+        {
+            user_id: expect.any(String),
+            pet_id: expect.any(String),
+            name: expect.any(String),
+            species: expect.any(Boolean),
+            breed: expect.any(String),
+            age: expect.any(Number),
+            weight: expect.any(Number),
+        },
+      ],
+    });
+  });
+  
+});  
