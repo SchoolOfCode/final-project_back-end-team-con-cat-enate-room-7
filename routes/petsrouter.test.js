712de059bc5edd7351900240b69483a9053e12bd
JackC91 <jackjcherry@hotmail.co.uk>
2022-07-28 11:18:45 +0100

    get test working, issue with open handles, working on debug

--- routes/petsrouter.test.js
+++ routes/petsrouter.test.js
@@ -1,6 +1,15 @@
-import supertest from "supertest";
-import { Express } from "express";
+import request from "supertest";
+// import  express  from "express";
 import { describe, it, expect } from "@jest/globals";
-import assert from "assert";
+import app from "../app.js"
 
-const app = express();
+
+// const app = express();
+
+await new Promise((resolve) => setTimeout(() => resolve(), 2000));
+describe('get request from pets route, returning all data from DB and response code/message', function() {
+    it('responds 200 upon request', async function() {
+      const response = await request(app).get('/pets')
+      expect(response.statusCode).toEqual(200);
+    });
+  });
