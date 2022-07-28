62b01a2064c40e74df7985b3d615095a974f4b14
JackC91 <jackjcherry@hotmail.co.uk>
2022-07-28 11:35:04 +0100

    pets get test working and confirmed to be error free

--- routes/petsrouter.test.js
+++ routes/petsrouter.test.js
@@ -1,13 +1,9 @@
 import request from "supertest";
-// import  express  from "express";
-import { describe, it, expect } from "@jest/globals";
+import { describe, test, expect } from "@jest/globals";
 import app from "../app.js"
 
-
-// const app = express();
-
 describe('get request from pets route, returning all data from DB and response code/message', function() {
-    it('responds 200 upon request', async function() {
+    test('responds 200 upon request', async function() {
         await new Promise((resolve) => setTimeout(() => resolve(), 2000));
       const response = await request(app).get('/pets')
       expect(response.statusCode).toEqual(200);
